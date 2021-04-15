f45e5baf9eeed5c7504aa076384d86c6bddb8542
kateleib <kateleib@Kates-MacBook-Pro.local>
2021-04-14 21:49:06 -0400

    progress

--- Kate/Kate-GetData.py
+++ Kate/Kate-GetData.py
@@ -49,8 +49,12 @@ def createWarriorsTable():
         # From here loop through each game in dict_list and add the game_id, home team score, and visitor
         # team score into the database
         for game in dict_list:
-            continue #replace this with code
-
+            #replace this with code
+            game_id = i['id']
+            home_score = i['home_team_score']
+            visitor_score = i['visitor_team_score']
+            cur.execute('INSERT INTO Warrior (game_id, home_team_score, visitor_team_score) VALUES (?, ?, ?)', (game_id, home_score, visitor_score))
+            conn.commit()
 
         conn.commit()     #commit the changes
         print('Taking a short break...')
@@ -67,6 +71,30 @@ def createSixersTable():
     # Do same thing as above but for 76ers (team_ids[]=23) in url now
     # make sure to create new table for them
 
+    cur, conn = setUpDatabase('balldontlie.db')
+    cur.execute('CREATE TABLE IN NOT EXISTS Sixers (game_id INTEGER UNIQUE, home_team_score INTEGER, away_team_score)')
+    base_url = 'https://balldontlie.io/api/v1/games?seasons[]=2018&team_ids[]=23&seasons[]=2017&postseason=false&page={}'
+
+    # Each Request returns 25 games
+    # There are 7 pages
+    for i in range(1,8):
+        print(f'Retreieving games from page {i}')
+        request_url = base_url.format(i)
+        r = requests.get(request_url)
+        data = r.text
+        dict_list = json.loads(data)['data']
+
+        # Look at example.json to see what dict_list will look like for each iteration
+        # From here loop through each game in dict_list and add the game_id, home team score, and visitor
+        # team score into the database
+        for game in dict_list:
+            #replace this with code
+            game_id = i['id']
+            home_score = i['home_team_score']
+            visitor_score = i['visitor_team_score']
+            cur.execute('INSERT INTO Sixers (game_id, home_team_score, visitor_team_score) VALUES (?, ?, ?)', (game_id, home_score, visitor_score))
+            conn.commit()
+
 
 def createRocketsTable():
     '''
@@ -77,6 +105,29 @@ def createRocketsTable():
 
     # Do same thing as above but for 76ers (team_ids[]=10) in url now
     # make sure to create new table for them
+    cur, conn = setUpDatabase('balldontlie.db')
+    cur.execute('CREATE TABLE IN NOT EXISTS Rockets (game_id INTEGER UNIQUE, home_team_score INTEGER, away_team_score)')
+    base_url = 'https://balldontlie.io/api/v1/games?seasons[]=2018&team_ids[]=10&seasons[]=2017&postseason=false&page={}'
+
+    # Each Request returns 25 games
+    # There are 7 pages
+    for i in range(1,8):
+        print(f'Retreieving games from page {i}')
+        request_url = base_url.format(i)
+        r = requests.get(request_url)
+        data = r.text
+        dict_list = json.loads(data)['data']
+
+        # Look at example.json to see what dict_list will look like for each iteration
+        # From here loop through each game in dict_list and add the game_id, home team score, and visitor
+        # team score into the database
+        for game in dict_list:
+            #replace this with code
+            game_id = i['id']
+            home_score = i['home_team_score']
+            visitor_score = i['visitor_team_score']
+            cur.execute('INSERT INTO Warrior (game_id, home_team_score, visitor_team_score) VALUES (?, ?, ?)', (game_id, home_score, visitor_score))
+            conn.commit()
 
 
 # uncomment below lines when done
