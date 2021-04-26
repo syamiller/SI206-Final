14ccb01c2f4e323e25f09364604b8c792b5d2071
syamiller <symiller@umich.edu>
2021-04-25 20:44:26 -0400

    for Evan

--- Evan/Evan-ReadData.py
+++ Evan/Evan-ReadData.py
@@ -8,17 +8,32 @@ def setUpDatabase(db_name):
     Create the database and return the cursor and connection objects.
     Used in function to update databses
     '''
-    path = os.path.dirname(os.path.abspath(__file__))
-    conn = sqlite3.connect(path+'/'+db_name)
+    conn = sqlite3.connect(db_name)
     cur = conn.cursor()
     return cur, conn
 
-def doCalc():
+def doCalc(filename):
     '''
     Do the Calculations:
     - Get number of players from each country
     '''
-    pass
+    with open(filename, 'r') as jsonFile:
+        data = json.load(filename)
+    
+
+    cur, conn = setUpDatabase('balldontlie.db')
+    id_list = cur.execute('SELECT * FROM Players').fetchall()
+
+    counts_dict = {}
+
+    for tup in id_list:
+        counts_dict[tup[2]] = counts_dict.get(tup[2], 0) + 1
+
+    data['Soccer'] = {} # sorted dictionary
+
+    with open(filename, 'w') as f:
+        json.dump(data, f)    
+    
 
 def showViz():
     '''
@@ -28,3 +43,6 @@ def showViz():
     - number of players on y axis
     '''
     pass
+
+if __name__ == '__main__':
+    doCalc('data.json')
