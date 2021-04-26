374e92225ebf19da8e090d26a1a23305d16f7db4
evanras <evanras@umich.edu>
2021-04-25 22:46:01 -0400

    changed the dictionary to have percents as values rather than numbers to make the pie chart easier

--- Evan/Evan-ReadData.py
+++ Evan/Evan-ReadData.py
@@ -17,8 +17,8 @@ def doCalc(filename):
     Do the Calculations:
     - Get number of players from each country
     '''
-    with open(filename, 'r') as jsonFile:
-        data = json.load(filename)
+    #with open(filename, 'r') as jsonFile:
+     #   data = json.load(jsonFile)
     
 
     cur, conn = setUpDatabase('balldontlie.db')
@@ -28,24 +28,31 @@ def doCalc(filename):
 
     for tup in id_list:
         counts_dict[tup[2]] = counts_dict.get(tup[2], 0) + 1
-    print(counts_dict)
+    #print(counts_dict)
     sorted_countries = sorted(counts_dict.values(), reverse = True)
-    print(sorted_countries)
+    #print(sorted_countries)
     sorted_counts_dict = {}
+    total = 0
+    for i in sorted_countries:
+        total += i
+    percents = []
+    for i in sorted_countries: 
+        percents.append(i/total*100)
+    percents[0]=percents[0] - .000000000000004
+    print(percents)
+    count = -1
     for j in sorted_countries:
+        count+= 1
         for i in counts_dict:
             if counts_dict[i] == j:
-                sorted_counts_dict[i] = j
-        #count = 0
-        #sorted_counts_dict[i] = sorted_countries[count]
-        #count+= 1
+                sorted_counts_dict[i] = percents[count]
+        
     print(sorted_counts_dict)
     
+    #data['Soccer'] = sorted_counts_dict 
 
-    data['Soccer'] = sorted_counts_dict 
-
-    with open(filename, 'w') as f:
-        json.dump(data, f)    
+    #with open(filename, 'w') as f:
+    #    json.dump(data, f)    
     
 
 def showViz():
@@ -55,7 +62,11 @@ def showViz():
     - country name on x axis
     - number of players on y axis
     '''
-    pass
+    with open(filename, 'r') as jsonFile:
+        data = json.load(jsonFile)
+
+    data = data["Soccer"]
+
 
 if __name__ == '__main__':
     doCalc('data.json')
