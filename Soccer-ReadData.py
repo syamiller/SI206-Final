f8e6659d2e3f9627196fe32436ef3f4ac60d8f7a
evanras <evanras@umich.edu>
2021-04-26 16:44:16 -0400

    viz is complete

--- Soccer-ReadData.py
+++ Soccer-ReadData.py
@@ -38,37 +38,30 @@ def doCalc(filename):
     goalie_count = 0
     for val in player_heights:
         if val[2] == 0:
-            #attacker
             attacker += val[1]
             attacker_count += 1
         elif val[2] == 1:
-            #Midfielder
             mid += val[1]
             mid_count += 1
         elif val[2] == 2:
-            #Defender
             defender += val[1]
             defender_count += 1
         else:
-            #Goalkeeper
             goalie += val[1]
             goalie_count += 1
     avg_attacker = float(str(attacker/attacker_count)[:6])
     avg_mid = float(str(mid/mid_count)[:6])
     avg_defender = float(str(defender/defender_count)[:6])
     avg_goalie = float(str(goalie/goalie_count)[:6])
-    print(avg_attacker, avg_defender, avg_goalie, avg_mid)
+    #print(avg_attacker, avg_defender, avg_goalie, avg_mid)
     
     avgs_d = {}
     avgs_d["Attacker"] = avg_attacker
     avgs_d["Midfielder"] = avg_mid
     avgs_d["Defender"] = avg_defender
     avgs_d["Goalkeeper"] = avg_goalie
-    print(avgs_d)
+    #print(avgs_d)
 
-
-
-        
     data['Soccer'] = avgs_d
 
     with open(filename, 'w') as f:
@@ -84,14 +77,16 @@ def showViz(filename):
     with open(filename, 'r') as jsonFile:
         data = json.load(jsonFile)
     
-    sizes = data['Soccer'].values()
-    labels = data['Soccer'].keys()
+    heights = data['Soccer'].values()
+    positions = data['Soccer'].keys()
 
-    fig1, ax1 = plt.subplots()
+    plt.bar(positions, heights, align='center', alpha=0.5)
+    plt.ylabel('Average Heights')
+    plt.xlabel('Positions')
+    plt.title('Average Heights By Position')
 
-    ax1.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
     plt.show()
 
 if __name__ == '__main__':
-    doCalc('data.json')
-    #showViz('data.json')
+    #doCalc('data.json')
+    showViz('data.json')
