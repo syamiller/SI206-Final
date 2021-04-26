212cb84f31b2e1762c86f05ecff4de8b571648f1
evanras <evanras@umich.edu>
2021-04-25 23:23:15 -0400

    pie chart should be about ready to go once I figure out why it says data.json doesn't exist

--- Evan/Evan-ReadData.py
+++ Evan/Evan-ReadData.py
@@ -1,6 +1,8 @@
-import matplotlib
+import matplotlib as plt
 import sqlite3
 import os
+import json
+import numpy as np
 
 
 def setUpDatabase(db_name):
@@ -17,8 +19,8 @@ def doCalc(filename):
     Do the Calculations:
     - Get number of players from each country
     '''
-    #with open(filename, 'r') as jsonFile:
-     #   data = json.load(jsonFile)
+    with open(filename, 'r') as jsonFile:
+        data = json.load(jsonFile)
     
 
     cur, conn = setUpDatabase('balldontlie.db')
@@ -49,10 +51,10 @@ def doCalc(filename):
         
     print(sorted_counts_dict)
     
-    #data['Soccer'] = sorted_counts_dict 
+    data['Soccer'] = sorted_counts_dict 
 
-    #with open(filename, 'w') as f:
-    #    json.dump(data, f)    
+    with open(filename, 'w') as f:
+        json.dump(data, f)    
     
 
 def showViz():
@@ -62,11 +64,24 @@ def showViz():
     - country name on x axis
     - number of players on y axis
     '''
-    with open(filename, 'r') as jsonFile:
-        data = json.load(jsonFile)
+    cur, conn = setUpDatabase('balldontlie.db')
+    id_list = cur.execute('SELECT * FROM Countries').fetchall()
+    countries_list = []
+    
 
-    data = data["Soccer"]
+    #with open(filename, 'r') as jsonFile:
+    #    data = json.load(jsonFile)
+
+    #data = data["Soccer"]
+    y = np.array(data.values())
+    #labels = need to go into countries table to get their names 
+
+    plt.pie(y, labels = mylabels, shadow = True)
+    plt.show()
+
+
 
 
 if __name__ == '__main__':
     doCalc('data.json')
+    #showViz()
